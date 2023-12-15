487613bb70dcabfb08bcd65358a1e4042215ef38
gines <ggutierrezs01@iescastelar.com>
2023-12-15 10:04:59 +0100

    BD practica3 a medias

---
+++ SQL/GutiérrezSaura_Ginés_Creacion_P3.sql
@@ -0,0 +1,45 @@
+CREATE TABLE EMPLEADOS(
+	dni VARCHAR(10) PRIMARY KEY,
+	nombre VARCHAR(20) NOT NULL,
+	apellido1 VARCHAR(15) NOT NULL,
+	apellido2 VARCHAR(15) NOT NULL,
+	direcc VARCHAR(25),
+	ciudad VARCHAR(20),
+	codPostal VARCHAR(5),
+	sexo ENUM('h','n') NOT NULL,
+	fecha_nac DATE
+);
+
+CREATE TABLE TRABAJOS(
+	codigo VARCHAR(5),
+	nombre VARCHAR(10) UNIQUE,
+	salario DECIMAL(6,2) CHECK (salario<=5000)
+);
+
+CREATE TABLE UNIVERSIDADES(
+	univ_od VARCHAR(5),
+	nombre_univ VARCHAR(25),
+	ciudad VARCHAR(20),
+	cod_postal VARCHAR(5)
+);
+
+CREATE TABLE DEPARTAMENTOS(
+	dptoCod INT AUTO_INCREMENT,
+	nombre_dpto VARCHAR(30) UNIQUE NOT NULL,
+	jefe VARCHAR(10),
+	presupuesto DECIMAL(7,2) NOT NULL,
+	PRIMARY KEY(dptoCod),
+	FOREIGN KEY (jefe) REFERENCES EMPLEADOS(dni)
+);
+
+CREATE TABLE ESTUDIOS(
+	dni_emple VARCHAR(10),
+	numero INT,
+	universidad VARCHAR(5),
+	titulacion VARCHAR(30),
+	especialidad VARCHAR(20),
+	anio INT(4),
+	PRIMARY KEY(dni_emple, numero),
+	FOREIGN KEY(dni_emple) REFERENCES EMPLEADOS(dni) ON DELETE CASCADE,
+	FOREIGN KEY(universidad) REFERENCES UNIVERSIDADES(univ_cod) ON DELETE CASCADE
+);
