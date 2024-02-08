d1adf15046b314ef82be6d57885958cf5ff31885
Gineeesss <ggutierrezs01@iescastelar.com>
2024-02-08 19:05:29 +0100

    practicaddl y consutlas410 18

--- SQL/GutiérrezSaura_Ginés_consultas410_18.sql
+++ SQL/GutiérrezSaura_Ginés_consultas410_18.sql
@@ -82,28 +82,69 @@ cando dónde está la oficina.*/
 SELECT c.NOmbreCliente, o.Ciudad
 FROM Clientes c INNER JOIN Oficinas o USING(Ciudad);
 
+/*# 12. Sacar los clientes que residan en ciudades donde no hay oficinas ordenado por la ciudad donde residen.*/
+
+SELECT c.CodigoCliente, c.NOmbreCliente, c.Ciudad
+FROM Clientes c LEFT JOIN Oficinas o USING(Ciudad)
+WHERE o.CodigoOficina IS NULL
+ORDER BY c.Ciudad;
+
 /*# 13. Sacar el número de clientes que tiene asignado cada representante de ventas.*/
 
-SELECT DISTINCT e.Nombre, COUNT(c.NombreCliente) AS "Total CLientes Atendidos"
-FROM Clientes c  JOIN Empleados e ON c.CodigoEmpleadoRepVentas = e.CodigoEmpleado;
-
-/*# 12. Sacar los clientes que residan en ciudades donde no hay oficinas ordenado por*/
-
-/*# la ciudad donde residen.
-*/
+SELECT e.CodigoEmpleado, e.Nombre, COUNT(c.CodigoCliente) as "Total Clientes Asociados"
+FROM Empleados e LEFT JOIN Clientes c ON e.CodigoEmpleado = c.CodigoEmpleadoRepVentas
+GROUP BY e.CodigoEmpleado, e.Nombre;
 
 /*# 14. Sacar cuál fue el cliente que hizo el pago con mayor cuantía y el que hizo el
 pago con menor cuantía.
 */
 
-/*# 15. Sacar un listado con el precio total de cada pedido.
-*/
+SELECT c.CodigoCliente, c.NombreCliente, p.Cantidad AS MayorCuantia
+FROM Pagos p JOIN Clientes c USING(CodigoCliente)
+WHERE p.Cantidad = (
+    SELECT MAX(Cantidad)
+    FROM Pagos
+)
+UNION
+SELECT c.CodigoCliente, c.NombreCliente, p.Cantidad AS MenorCuantia
+FROM Pagos p JOIN Clientes c USING(CodigoCliente)
+WHERE p.Cantidad = (
+    SELECT MIN(Cantidad)
+    FROM Pagos
+);
+
+
+
+/*# 15. Sacar un listado con el precio total de cada pedido*/
+
+SELECT CodigoPedido,SUM(Cantidad * PrecioUnidad) AS PrecioTotal
+FROM DetallePedidos
+GROUP BY CodigoPedido;
+
 
 /*# 16. Sacar los clientes que hayan hecho pedidos en el 2008 por una cuantía superior
 a 2000 euros.
 */
 
+SELECT DISTINCT c.CodigoCliente, c.NombreCliente
+FROM Clientes c JOIN Pedidos p USING(CodigoCliente)
+JOIN DetallePedidos dp USING (CodigoPedido)
+WHERE YEAR(p.FechaPedido) = 2008
+GROUP BY c.CodigoCliente, c.NombreCliente
+HAVING SUM(dp.Cantidad * dp.PrecioUnidad) > 2000;
+
+
 /*# 17. Sacar cuántos pedidos tiene cada cliente en cada estado.
 */
 
+SELECT c.CodigoCliente, c.NombreCliente, p.Estado, COUNT(p.CodigoPedido) AS NumeroPedidos
+FROM Clientes c JOIN Pedidos p USING(CodigoCliente)
+GROUP BY c.CodigoCliente, p.Estado;
+
+
 /*# 18. Sacar los clientes que han pedido más de 200 unidades de cualquier producto*/
+
+SELECT DISTINCT c.CodigoCliente, c.NombreCliente
+FROM Clientes c JOIN Pedidos p USING(CodigoCliente)
+JOIN DetallePedidos dp USING(CodigoPedido)
+WHERE dp.Cantidad > 200;
