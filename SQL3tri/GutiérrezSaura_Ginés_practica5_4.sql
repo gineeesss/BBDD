c67980b1e3fe8ad1d116ed00e55b6232b25982dc
gines <ggutierrezs01@iescastelar.com>
2024-03-01 10:13:47 +0100

    ssdas

---
+++ SQL3tri/GutiérrezSaura_Ginés_practica5_4.sql
@@ -0,0 +1,20 @@
+alter table DetallePedidos add iva integer(2.2) default 18;
+
+START TRANSACTION;
+   update DetallePedidos set iva=18 where CodigoPedido in(select CodigoPedido from Pedidos where FechaPedido>='2010-07-01');
+   update DetallePedidos set iva=16 where CodigoPedido in(select CodigoPedido from Pedidos where FechaPedido<'2010-07-01');
+COMMIT WORK;
+
+START TRANSACTION;
+    alter table DetallePedidos add TotalLinea decimal(8,2);
+    update DetellePedidos set TotalLinea=((precioUnidad*Cantidad*iva)/100);
+COMMIT WORK;
+
+delete from Clientes where CodigoCliente=
+(select CodigoCliente from Pagos where LimiteCredito=
+(select min(LimiteCredito) from Clientes) Limit 1);
+
+
+CREATE TABLE Clientes2 LIKE Clientes; 
+insert into Clientes2
+select * from Clientes;
